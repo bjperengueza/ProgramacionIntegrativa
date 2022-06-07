94ec76a58628c034d49a64b7178922e3188fae54
bjperengueza <brandon.jperen@gmail.com>
2022-06-06 20:11:44 -0500

    botones del catalogo

---
+++ Proyecto_Grupo7/view.js
@@ -0,0 +1,68 @@
+function mostrar(){
+    document.getElementById('img1').style.display = 'block';
+    document.getElementById('img2').style.display = 'block';
+    document.getElementById('img3').style.display = 'block';
+    document.getElementById('img4').style.display = 'block';
+    document.getElementById('img5').style.display = 'block';
+    document.getElementById('img6').style.display = 'block';
+    document.getElementById('img7').style.display = 'none';
+    document.getElementById('img8').style.display = 'none';
+    document.getElementById('img9').style.display = 'none';
+    document.getElementById('img10').style.display = 'none';
+    document.getElementById('img11').style.display = 'none';
+    document.getElementById('img12').style.display = 'none';
+    document.getElementById('img13').style.display = 'none';
+    document.getElementById('img14').style.display = 'none';
+    document.getElementById('img15').style.display = 'none';
+    document.getElementById('img16').style.display = 'none';
+    document.getElementById('img17').style.display = 'none';
+    document.getElementById('img18').style.display = 'none';
+	
+}
+
+
+function mostrar1(){
+    document.getElementById('img1').style.display = 'none';
+    document.getElementById('img2').style.display = 'none';
+    document.getElementById('img3').style.display = 'none';
+    document.getElementById('img4').style.display = 'none';
+    document.getElementById('img5').style.display = 'none';
+    document.getElementById('img6').style.display = 'none';
+    document.getElementById('img7').style.display = 'block';
+    document.getElementById('img8').style.display = 'block';
+    document.getElementById('img9').style.display = 'block';
+    document.getElementById('img10').style.display = 'block';
+    document.getElementById('img11').style.display = 'block';
+    document.getElementById('img12').style.display = 'block';
+    document.getElementById('img13').style.display = 'none';
+    document.getElementById('img14').style.display = 'none';
+    document.getElementById('img15').style.display = 'none';
+    document.getElementById('img16').style.display = 'none';
+    document.getElementById('img17').style.display = 'none';
+    document.getElementById('img18').style.display = 'none';
+	
+}
+
+function mostrar2(){
+    document.getElementById('img1').style.display = 'none';
+    document.getElementById('img2').style.display = 'none';
+    document.getElementById('img3').style.display = 'none';
+    document.getElementById('img4').style.display = 'none';
+    document.getElementById('img5').style.display = 'none';
+    document.getElementById('img6').style.display = 'none';
+    document.getElementById('img7').style.display = 'none';
+    document.getElementById('img8').style.display = 'none';
+    document.getElementById('img9').style.display = 'none';
+    document.getElementById('img10').style.display = 'none';
+    document.getElementById('img11').style.display = 'none';
+    document.getElementById('img12').style.display = 'none';
+    document.getElementById('img13').style.display = 'block';
+    document.getElementById('img14').style.display = 'block';
+    document.getElementById('img15').style.display = 'block';
+    document.getElementById('img16').style.display = 'block';
+    document.getElementById('img17').style.display = 'block';
+    document.getElementById('img18').style.display = 'block';
+
+}
+
+  
